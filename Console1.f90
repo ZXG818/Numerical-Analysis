bf8e8c3b48a744f5df9fecce44b8c727b384dcfd
trace-shadow <37297081+trace-shadow@users.noreply.github.com>
2020-08-04 19:35:40 +0800

    Add files via upload

---
+++ Console1.f90
@@ -0,0 +1,74 @@
+    module NUMERICAL
+    implicit none
+        private zero        ! 此为私有变量
+        private GetPoint    ! 此函数为私有函数，进行封装
+        real, parameter :: zero = 0.00001
+    contains
+    
+        real function GetPoint(a, b, func)
+        implicit none
+            real, intent(in) :: a
+            real, intent(in) :: b
+            real, external :: func
+            
+            real :: FA
+            real :: FB
+            real :: k
+            
+            FA = func(a)
+            FB = func(b)
+            k = (FB-FA) / (b-a)
+            
+            GetPoint = a - FA/k
+            return 
+        end function
+    
+
+        real function Solver(a, b, func)
+        implicit none
+            ! 声明参数
+            real, intent(inout) :: a
+            real, intent(inout) :: b
+            real, external :: func  ! 声明外部函数，进行调用
+            ! 声明函数内临时变量
+            real :: c
+            real :: FA
+            real :: FB
+            real :: FC
+            
+            c = GetPoint(a, b, func)
+            FA = func(a)
+            FB = func(b)
+            FC = func(c)
+            
+            do while(abs(FC - 0) >= zero)
+                a = b
+                FA = FB
+                b = c
+                FB = FC
+                c = GetPoint(a, b, func)
+                FC = func(c)
+            end do
+            
+            Solver = c
+            return
+        end function
+        
+        real function func(X)
+        implicit none
+            real, intent(in) :: X
+            func = sin(X)
+            return
+        end function
+    end module
+    
+    program main
+    use NUMERICAL
+    implicit none
+        real :: a, b
+        real :: ans
+        write(*, *) "输入两个猜测的数值:"
+        read(*, *) a, b
+        ans = Solver(a, b, func)
+        write(*, *) ans
+    end program
