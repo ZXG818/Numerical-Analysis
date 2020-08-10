44d93142cdf58e1b8fbb4077c595e3b6a34223f5
trace-shadow <37297081+trace-shadow@users.noreply.github.com>
2020-08-10 23:22:56 +0800

    Rename Console1.f90 to Inverse_matrix.f90
    利用求解矩阵方程来求得逆矩阵。

--- Inverse_matrix.f90
+++ Inverse_matrix.f90
@@ -5,7 +5,7 @@
         PUBLIC SHOWMAT
         PUBLIC INVERSE_MAT
     CONTAINS
-        ! ��ӡ����
+        ! 打印矩阵
         SUBROUTINE SHOWMAT(MAT)
         IMPLICIT NONE
             REAL, INTENT(IN) :: MAT(:, :)
@@ -16,15 +16,15 @@
             RETURN 
         END SUBROUTINE
         
-        ! �����ǻ�ϵ������ͬʱ�ԵȺ��ұ���������ͬ���б任
+        ! 上三角化系数矩阵，同时对等号右边列向量做同等行变换
         SUBROUTINE UPPER(MAT, E)
         IMPLICIT NONE
             REAL, INTENT(INOUT) :: MAT(:, :)
-            REAL, INTENT(INOUT) :: E(:, :) ! ��λ����
+            REAL, INTENT(INOUT) :: E(:, :) ! 单位矩阵
             INTEGER :: I, J
             DO I=1, SIZE(MAT, 2)-1
                 DO J=I+1, SIZE(MAT, 2)
-                    ! ע�⣬100�����200����˳���ܵ�����
+                    ! 注意，100语句与200语句的顺序不能调换！
 100                 E(:, J) = E(:, J) - E(:, I) * MAT(I, J) / MAT(I, I)               
 200                 MAT(:, J) = MAT(:, J) - MAT(I, J) * MAT(:, I) / MAT(I, I)
                 END DO
@@ -32,16 +32,16 @@
             RETURN
         END SUBROUTINE
         
-        ! �����ǻ�ϵ������ͬʱ�ԵȺ��ұ���������ͬ���б任
+        ! 下三角化系数矩阵，同时对等号右边列向量做同等行变换
         SUBROUTINE LOWER(MAT, E)
         IMPLICIT NONE
             REAL, INTENT(INOUT) :: MAT(:, :)
             REAL, INTENT(INOUT) :: E(:, :)
             INTEGER :: I, J
-            ! ���о�����б任
+            ! 进行矩阵的行变换
             DO I=SIZE(MAT, 2), 2, -1
                 DO J=I-1, 1, -1
-                    ! ע�⣬300�����400����˳���ܵ�����
+                    ! 注意，300语句与400语句的顺序不能调换！
 300                 E(:, J) = E(:, J) - E(:, I) * MAT(I, J) / MAT(I, I)
 400                 MAT(:, J) = MAT(:, J) - MAT(I, J) * MAT(:, I) / MAT(I, I)
                 END DO
@@ -49,10 +49,10 @@
             RETURN
         END SUBROUTINE
         
-        ! �������
+        ! 求逆矩阵
         SUBROUTINE INVERSE_MAT(MAT, E)
         IMPLICIT NONE
-            REAL, INTENT(INOUT) :: MAT(:, :) ! ϵ������
+            REAL, INTENT(INOUT) :: MAT(:, :) ! 系数矩阵
             REAL, INTENT(INOUT) :: E(:, :)
             INTEGER :: I, J
             CALL SHOWMAT(MAT)
@@ -63,7 +63,7 @@
             CALL LOWER(MAT, E)
             CALL SHOWMAT(MAT)
             WRITE(*, *) "------------------"
-            ! �ó�MAT�������
+            ! 得出MAT的逆矩阵
             FORALL(I=1:SIZE(MAT, 2)) E(:, I) = E(:, I) / MAT(I, I)
             RETURN 
         END SUBROUTINE    
@@ -77,12 +77,12 @@
         INTEGER :: I, J
         REAL :: E(3, 3)
         
-        ! ���ɵ�λ����
+        ! 生成单位矩阵
         FORALL(I=1:3, J=1:3, I==J) E(I, J) = 1.0
         FORALL(I=1:3, J=1:3, I/=J) E(I, J) = 0.0
         
         CALL INVERSE_MAT(MAT, E)
         CALL SHOWMAT(E)
-        WRITE(*, *) MATMUL(BACKUP, E)   ! ʹ��ԭ��������õ��������г˻��������Ƿ��ǵ�λ����
+        WRITE(*, *) MATMUL(BACKUP, E)   ! 使用原矩阵与求得的逆矩阵进行乘积，看看是否是单位矩阵
     END PROGRAM
     
